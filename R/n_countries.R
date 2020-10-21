248a503b2b35d728374d780b3533474d5d030996
chenjie <gcjbest@icloud.com>
2020-10-21 23:58:08 +0800

    n_country

--- R/n_countries.R
+++ R/n_countries.R
@@ -1,9 +1,9 @@
-
 #' count the number of countried in dataset
-#'
+#' @title n_countries
+#' @return sum of country during this epdemic
 #' @param data a dataset containing a `country` column
-#'
-#'@examples
+#' @description it help people count the country
+#' @examples
 #' n_countries(coronavirus)
 #'
 #' @return sum of country during this epdemic 
