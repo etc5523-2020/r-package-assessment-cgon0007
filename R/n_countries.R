454d13b8f7df221806cf76994244eb0c2b151230
chenjie <gcjbest@icloud.com>
2020-10-21 20:06:07 +0800

    n_country

--- R/n_countries.R
+++ R/n_countries.R
@@ -1,11 +1,15 @@
+
 #' count the number of countried in dataset
 #'
 #' @param data a dataset containing a `country` column
 #'
-#' @return
-#' @export
+#'@examples
+#' n_countries(coronavirus)
 #'
-#' @examples
+#' @return sum of country during this epdemic 
+#' @export
 n_countries<-function(data){
   dplyr::n_distinct(data$country)
 }
+
+
